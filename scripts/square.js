4044938e9f3a76140d69a6d3022563d84d92bc80
Prashanth <prashanthr036@gmail.com>
2019-02-21 21:50:56 -0600

    update

--- scripts/square.js
+++ scripts/square.js
@@ -2,7 +2,7 @@ function myfunction(){
    let side =  document.getElementById("myid").value;
    let area = parseInt(side) * parseInt(side);
  
-  alert(document.getElementById("myPara").innerHTML=area);
-  
+    alert(document.getElementById("myPara").innerHTML=area);
+ 
 
 }
